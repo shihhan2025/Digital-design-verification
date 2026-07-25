52f3314e6d30b01daebe2995577fece0d4977aa3
shchang <shchang.ee09@nycu.edu.tw>
2026-07-25 16:26:55 +0800

    Update fifo_if.sv

--- FIFO/testbench/interface/fifo_if.sv
+++ FIFO/testbench/interface/fifo_if.sv
@@ -1,1 +1,15 @@
-
+interface fifo_if #(parameter DATA_WIDTH = 8)
+(
+    input logic clk
+);
+    logic rst_n;
+    // write side
+    logic wr_en;
+    logic [DATA_WIDTH-1:0] data_in;
+    // read side
+    logic rd_en;
+    logic [DATA_WIDTH-1:0] data_out;
+    // status
+    logic empty;
+    logic full;
+endinterface
