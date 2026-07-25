bbbc0632f6d9bb55b1e1f087a99b9a18131ee3d2
shchang <shchang.ee09@nycu.edu.tw>
2026-07-25 16:36:53 +0800

    Update fifo_transaction.sv

--- FIFO/testbench/transaction/fifo_transaction.sv
+++ FIFO/testbench/transaction/fifo_transaction.sv
@@ -1,9 +1,9 @@
-class fifo_transaction extends uvm_sequence_item;
+class fifo_transaction #(parameter FIFO_WIDTH=8) extends uvm_sequence_item;
 
 rand bit wr_en;
 rand bit rd_en;
-rand bit [7:0] data_in;
-bit [7:0] data_out;
+rand bit [FIFO_WIDTH-1:0] data_in;
+bit [FIFO_WIDTH-1:0] data_out;
 
 `uvm_object_utils(fifo_transaction)
 
