0e0d15a5300345113c38ab993603f0ecd749b5a5
shchang <shchang.ee09@nycu.edu.tw>
2026-07-25 16:54:38 +0800

    Update fifo_transaction.sv

--- FIFO/testbench/transaction/fifo_transaction.sv
+++ FIFO/testbench/transaction/fifo_transaction.sv
@@ -1,11 +1,25 @@
-class fifo_transaction #(parameter FIFO_WIDTH=8) extends uvm_sequence_item;
+class fifo_transaction #(parameter DATA_WIDTH=8) extends uvm_sequence_item;
 
 rand bit wr_en;
 rand bit rd_en;
-rand bit [FIFO_WIDTH-1:0] data_in;
-bit [FIFO_WIDTH-1:0] data_out;
+rand bit [DATA_WIDTH-1:0] data_in;
 
-`uvm_object_utils(fifo_transaction)
+bit [DATA_WIDTH-1:0] data_out;
+bit full;
+bit empty;
+
+`uvm_object_utils_begin(fifo_seq_item)
+  `uvm_field_int(wr_en,    UVM_ALL_ON)
+  `uvm_field_int(rd_en,    UVM_ALL_ON)
+  `uvm_field_int(data_in,  UVM_ALL_ON)
+  `uvm_field_int(data_out, UVM_ALL_ON)
+  `uvm_field_int(full,     UVM_ALL_ON)
+  `uvm_field_int(empty,    UVM_ALL_ON)
+`uvm_object_utils_end
+
+// constraint c_op { 
+  
+//}
 
 function new(string name="fifo_seq_item");
   super.new(name);
