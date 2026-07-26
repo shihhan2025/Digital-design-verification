da057483a8258adaec3d58f3bec5eaf5f25e9775
shchang <shchang.ee09@nycu.edu.tw>
2026-07-27 00:04:08 +0800

    Update fifo_base_test.sv

--- FIFO/testbench/test/fifo_base_test.sv
+++ FIFO/testbench/test/fifo_base_test.sv
@@ -1,1 +1,26 @@
+`ifndef FIFO_BASE_TEST_SV
+`define FIFO_BASE_TEST_SV
 
+class fifo_base_test extends uvm_test;
+  `uvm_component_utils(fifo_base_test)
+
+  fifo_env env;
+
+  function new(string name="fifo_base_test", uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = fifo_env::type_id::create("env", this);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);   
+    phase.raise_objection(this);
+    // wait reset finish
+    #100;
+    phase.drop_objection(this);
+  endtask
+
+endclass
+`endif
