b06778910b9886d3a3d96c2d70909a11c4502ba6
shchang <shchang.ee09@nycu.edu.tw>
2026-07-26 13:44:49 +0800

    Update fifo_sequencer.sv

--- FIFO/testbench/agent/fifo_sequencer.sv
+++ FIFO/testbench/agent/fifo_sequencer.sv
@@ -1,1 +1,11 @@
+`ifndef FIFO_SEQUENCER_SV
+`define FIFO_SEQUENCER_SV
+class fifo_sequencer extends uvm_sequencer #(fifo_seq_item);
+  `uvm_component_utils(fifo_sequencer)
 
+  function new(string name="fifo_sequencer", uvm_component parent=null);
+    super.new(name, parent);
+  endfunction
+endclass
+
+`endif
