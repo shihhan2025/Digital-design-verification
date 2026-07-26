ed57c9e21598970439a452bcd90e4026cb145103
shchang <shchang.ee09@nycu.edu.tw>
2026-07-26 23:39:44 +0800

    Update fifo_env.sv

--- FIFO/testbench/fifo_env.sv
+++ FIFO/testbench/fifo_env.sv
@@ -1,1 +1,26 @@
+`ifndef FIFO_ENV_SV
+`define FIFO_ENV_SV
 
+class fifo_env extends uvm_env;
+  `uvm_component_utils(fifo_env)
+
+  fifo_agent      agent;
+  fifo_scoreboard scoreboard;
+  
+  function new(string name = "fifo_env", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    agent = fifo_agent::type_id::create("agent", this);
+    scoreboard = fifo_scoreboard::type_id::create("scoreboard", this);
+  endfunction
+
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    agent.monitor.port.connect(scoreboard.item_imp);
+  endfunction
+endclass
+
+`endif
