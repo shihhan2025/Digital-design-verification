1130ac17881869e77662e8a798b9a130c2bedf6b
shchang <shchang.ee09@nycu.edu.tw>
2026-07-26 23:35:49 +0800

    Update fifo_pkg.sv

--- FIFO/testbench/pkg/fifo_pkg.sv
+++ FIFO/testbench/pkg/fifo_pkg.sv
@@ -1,1 +1,19 @@
+`ifndef FIFO_PKG_SV
+`define FIFO_PKG_SV
 
+package fifo_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  
+  `include "fifo_transaction.sv"
+  `include "fifo_sequence.sv" 
+  `include "fifo_driver.sv"
+  `include "fifo_monitor.sv"
+  `include "fifo_sequencer.sv"
+  `include "fifo_agent.sv"
+  `include "fifo_scoreboard.sv"
+  `include "fifo_env.sv"
+  `include "fifo_base_test.sv"
+endpackage : fifo_pkg
+
+`endif // FIFO_PKG_SV
