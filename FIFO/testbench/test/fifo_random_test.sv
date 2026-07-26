026d5bc1bafad989482fb32e8e69057f5e22a724
shchang <shchang.ee09@nycu.edu.tw>
2026-07-27 00:05:48 +0800

    Create fifo_random_test.sv

---
+++ FIFO/testbench/test/fifo_random_test.sv
@@ -0,0 +1,20 @@
+`ifndef FIFO_RANDOM_TEST_SV
+`define FIFO_RANDOM_TEST_SV
+
+class fifo_random_test extends fifo_base_test;
+  `uvm_component_utils(fifo_random_test)
+  fifo_random_sequence seq;
+  
+  function new(string name="fifo_random_test", uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    seq = fifo_random_sequence::type_id::create("seq");
+    seq.start(env.agent.sequencer);
+    phase.drop_objection(this);
+  endtask
+  
+endclass
+`endif
