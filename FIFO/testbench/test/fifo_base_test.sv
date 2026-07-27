95a3923d31d738b9bd6b8ea56b1b4acab686b580
shchang <shchang.ee09@nycu.edu.tw>
2026-07-27 12:03:50 +0800

    Update fifo_base_test.sv

--- FIFO/testbench/test/fifo_base_test.sv
+++ FIFO/testbench/test/fifo_base_test.sv
@@ -2,25 +2,20 @@
 `define FIFO_BASE_TEST_SV
 
 class fifo_base_test extends uvm_test;
-  `uvm_component_utils(fifo_base_test)
+    `uvm_component_utils(fifo_base_test)
+    fifo_env env;
+    function new(string name="fifo_base_test", uvm_component parent=null);
+      super.new(name,parent);
+    endfunction
+    virtual function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+      env = fifo_env::type_id::create("env", this);
+    endfunction
 
-  fifo_env env;
-
-  function new(string name="fifo_base_test", uvm_component parent=null);
-    super.new(name,parent);
-  endfunction
-
-  virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    env = fifo_env::type_id::create("env", this);
-  endfunction
-
-  virtual task run_phase(uvm_phase phase);   
-    phase.raise_objection(this);
-    // wait reset finish
-    #100;
-    phase.drop_objection(this);
-  endtask
+    virtual function void end_of_elaboration_phase(uvm_phase phase);
+        super.end_of_elaboration_phase(phase);
+        uvm_top.print_topology();
+    endfunction
 
 endclass
 `endif
